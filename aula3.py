56fcaefbd0cfb2c8e91afea2fd8fd899912b3be6
davidjwq <davidcaps001@gmail.com>
2025-09-18 17:53:43 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,37 @@
+#Calculadora
+
+
+# Soma
+
+Calculo1 = float(input('Calculo1_Soma: '))
+Calculo2 = float(input('Calculo2_Soma: '))
+print()
+Resultado = (Calculo1 + Calculo2)
+print (Resultado)
+print()
+# Subtração
+
+Calculo3 = float(input('Calculo1_Subtracao: '))
+Calculo4 = float(input('Calculo2_Subtracao: '))
+print()
+Resultado = (Calculo3 - Calculo4)
+print (Resultado)
+print()
+# Multiplicação
+
+Calculo5 = float(input('Calculo1_Multiplicacao: '))
+Calculo6 = float (input('calculo2_Multiplicacao: '))
+print()
+Resultado = (Calculo5 * Calculo6)
+print (Resultado)
+print()
+# Divisão
+
+Calculo7 = float(input('Calculo1_Divisao: '))
+Calculo8 = float (input('calculo2_Divisao: '))
+print()
+Resultado = (Calculo7 / Calculo8)
+print()
+print (Resultado)                       
+                        
+                        
