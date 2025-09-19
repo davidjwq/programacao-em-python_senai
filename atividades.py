016aec7c8fa8aa01cd1f7660e16d66c139a26d48
davidjwq <davidcaps001@gmail.com>
2025-09-19 16:59:13 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,68 @@
+
+nome = input('digite seu nome: ')
+sobrenome = input('digite seu sobrenome: ')
+print (nome, sobrenome)
+
+Digite = input('Digite Dois Numeros Inteiros. ')
+numero1 = float(input('Numero: - '))
+numero2 = float(input('Numero: - '))
+print ( Digite )
+
+Armazenar = input ('Digite A Palavra Armazenada: ')
+Numero_AoFinal = str(input('Digite o Numero A Ser Armazenado Junto: '))
+juntos = ( Armazenar + Numero_AoFinal)
+print (juntos)
+
+frase_escrita = input ('Ta Testando?')
+digite = ('Digite')
+resultado = (frase_Escrita + digite)
+print = (resultado)
+
+
+
+
+## ATIVIDADES:
+
+
+### 1 - Crie um programa para efetuar a leitura de um número inteiro e 
+# apresentar o resultado do quadrado deste número.
+
+
+numero = int(input('Digite um número: '))
+print(numero ** 2)
+
+
+### 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome.
+#  Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+
+nome = input('Digite o nome:  ')
+sobrenome =  input('Digite o sobrenome: ')
+print(nome, sobrenome)
+
+
+
+
+### 3 - Peça ao usuário para digitar dois números inteiros e armazene-os 
+# em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+
+
+numero_1 = int(input('numero1: '))
+numero_2 = int(input('numero2: '))
+
+
+print(numero_1, numero_2)
+
+
+
+### 4 - Crie uma ## ATIVIDADES:
+
+
+### 1 - Crie um programa para efetuar a leitura de um número inteiro e 
+# apresentar o resultado do quadrado deste número.
+
+
+numero = int(input('Digite um número: '))
+print(numero ** 2)
+
