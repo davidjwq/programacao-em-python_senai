eaab0044529ef8a3ee473bf558443aca4e819071
davidjwq <davidcaps001@gmail.com>
2025-09-24 16:36:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,89 @@
-# Utilize as 5 concatenações, teste todas 
+# Multiplique 4 por 6 e divida o resultado por 2. 
+# (Concatene com a palavra resultado)
 
-# 1 Imprima uma mensagem de boas-vindas na tela.
-print('Olá bom dia!')
-
-
-
-# 2 Declare uma variável booleana verdadeiro
-# com o valor True e imprima seu tipo
-
-boleano = True
-print(type(boleano))
+calculo  =  (4 * 6)/2
+print('Resultado', calculo)
 
 
-# 3 Imprima o resultado da multiplicação de dois 
-# números decimais de sua escolha
+# Calcule a soma de 5 e 7.
+# (Concatene com a palavra resultado)
 
-n1 = float(input('numero1:'))
-n2 = float(input('numero2:'))
+soma  =  5 + 7
+print('resultado', soma)
 
-print(n1 * n2)
+# Eleve 3 à potência de 4.
+# (Concatene com a palavra resultado)
 
-# 4 Imprima o resultado da divisão (/)de dois números 
-# inteiros de sua escolha.
+potencia  =  3 ** 4
+print('Resultado', potencia)
 
-n1 = int(input('numero1:'))
-n2 = int(input('numero2:'))
+# Calcule a média de 8, 12 e 15.
+# (Concatene com a palavra resultado)
 
-print(n1 / n2)
-
-# 5 Imprima o resultado da subtração de dois números 
-# inteiros de sua escolha
-
-n1 = int(input('numero1:'))
-n2 = int(input('numero2:'))
-
-print(n1 - n2)
+media  = (8 + 12 + 15)/3
+print('Resultado', media)
 
 
+# Subtraia 10 de 2 e multiplique o resultado por 
+# 5.(Concatene com a palavra resultado)
 
-# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros 
-# de sua escolha.
-
-
-n1 = int(input('numero1:'))
-n2 = int(input('numero2:'))
-
-print(n1 // n2)
-
-# 7 Imprima o resultado da multiplicação de 4 números 
-# decimais de sua escolha
-
-print(0.10 * 20.5 * 5.5  * 10.0)
+subtracao =  (2 - 10) * 5
+print('Resultado', subtracao)
 
 
-# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
-# imprima o dobro desse número
+# Divida 27 por 3 e some 5.
+# (Concatene com a palavra resultado)
 
-numero = 10
-dobro =  numero ** 2
-print(dobro)
-
-# 9 Crie um sistema de cadastro com as estruturas 
-# que vc já 
-# conhece(Use apenas input, print e variavel)
+# p(arentes)e(exponenciação)m(multiplicação)d(divisão)a(adição)s(soma) 
+div  =  27 / 3 + 5 
 
 
-nome = input('NOme: ')
-idade = int(input('Idade: '))
-endereco = input('Endereço:')
-curso =  input('Curso: ')
+# Exercícios com variáveis / print() / input()(opcional):
 
-print(f'''Dados:' 
-      
-      nome - {nome}
-      idade - {idade}
-      endereço - {endereco}
-      curso - {curso} 
+# Calcule o módulo ( % ) de 17 por 4.
+
+mod  = 17 % 4
+print('Resuldo módulo', mod)
 
 
+# Multiplique 9 por 3 e, em seguida, 
+# eleve o resultado ao quadrado.
+m = ( 9 * 3 ) ** 2
 
-''')
+
+# Calcule a raiz quadrada de 81
+
+raiz = 81 ** 0.5
+print('Resultado Raiz', raiz) 
+
+# Adicione 20 a 3 vezes 4.
+
+calculo = (20 + 3) * 4
+print('Resultado',calculo)
+
+
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+multiplicacao =  15 * 2 - 7
+
+# Eleve 5 ao cubo.
+
+cubo = 5 ** 3
+print('Resultado cubo', cubo)
+# Calcule a média de 17, 21 e 25.
+
+media2 = 17 + 21 + 25
+print('Resultado Multiplicação', media2/3)
+
+# Multiplique 11 por 2 e adicione 7.
+m = 11 * 2 + 7
+print('Resultado Multiplicação', m)
+
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+
+subt = (( 3 * 8 )-15)/2
+print('Resultado subtração', subt)
+
+# Eleve 2 à potência de 10.
+
+potencia = 2 ** 10
+print('Resultado',potencia)
